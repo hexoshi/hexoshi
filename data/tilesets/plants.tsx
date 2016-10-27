6a6719c165f7fa40250e28d5112e2af4970f7fd0
onpon4 <onpon4@riseup.net>
2016-10-27 13:18:34 -0400

    Added plantsfor decoration.

---
+++ data/tilesets/plants.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="plants" tilewidth="8" tileheight="8" tilecount="64">
+ <image source="../images/tiles/plants.png" width="256" height="16"/>
+</tileset>

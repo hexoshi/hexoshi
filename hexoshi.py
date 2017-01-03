25ab8cd98f937382586196f50944b1511d1f2f97
onpon4 <onpon4@riseup.net>
2017-01-03 15:56:42 -0500

    Updated the copyright notice.

--- hexoshi.py
+++ hexoshi.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
 # Hexoshi
-# Copyright (C) 2014-2016 Julie Marchant <onpon4@riseup.net>
+# Copyright (C) 2014-2017 Julie Marchant <onpon4@riseup.net>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

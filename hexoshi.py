49a433f778756b494e35193dbc8518923da24c83
Julie Marchant <onpon4@riseup.net>
2018-03-02 11:44:06 -0500

    version num update

--- hexoshi.py
+++ hexoshi.py
@@ -21,7 +21,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = "0.2"
+__version__ = "0.2.1a0"
 
 import argparse
 import datetime

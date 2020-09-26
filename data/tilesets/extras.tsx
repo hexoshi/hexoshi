6fa7ac988b70379d82da5510e42415bfd5e6864a
Layla Marchant <diligentcircle@riseup.net>
2020-09-25 21:17:42 -0400

    Converted all TMX and TSX files to JSON.

--- data/tilesets/extras.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="extras" tilewidth="8" tileheight="8" tilecount="77">
- <image source="../images/tiles/extras.png" width="88" height="56"/>
-</tileset>
--- data/tilesets/objects.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="objects" tilewidth="48" tileheight="8" tilecount="1">
- <tile id="0">
-  <properties>
-   <property name="cls" value="warp_pad"/>
-  </properties>
-  <image width="48" height="8" source="../images/objects/misc/warp_pad_inactive.png"/>
- </tile>
-</tileset>
